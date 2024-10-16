0255471ea4e6ac0d5930198e5b03b6a5a86570b5
Steband97 <sdelgadobarrios@gmail.com>
2024-10-16 17:49:43 -0500

    Mejoras en estilos y plantilla de mensajes recibidos

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const confirmButton = document.getElementById('confirm-button');
-  
-    if (localStorage.getItem('confirmed')) {
-      confirmButton.style.display = 'none';
-    }
-  
-    document.getElementById('modal-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      const name = document.getElementById('name-input').value;
-      const attendance = document.querySelector('input[name="attendance"]:checked').value;
-  
-      // Configura tus variables para EmailJS
-      const templateParams = {
-        user_name: name,
-        user_attendance: attendance
-      };
-  
-      // Enviar el correo electrónico usando EmailJS
-      emailjs.send('service_l78bsuf', 'template_91koe6d', templateParams)
-        .then(function(response) {
-          if (attendance === 'Sí') {
-            Swal.fire('¡Gracias, ' + name + '!', 'Nos alegra saber que asistirás a nuestra boda.', 'success').then(() => {
-              const modalElement = document.getElementById('Modal-Asistencia');
-              const modal = bootstrap.Modal.getInstance(modalElement);
-              modal.hide();
-              confirmButton.style.display = 'none';
-              localStorage.setItem('confirmed', true);
-            });
-          } else {
-            Swal.fire('¡Gracias, ' + name + '!', 'Lamentamos que no puedas asistir. ¡Te echaremos de menos!', 'error').then(() => {
-              const modalElement = document.getElementById('Modal-Asistencia');
-              const modal = bootstrap.Modal.getInstance(modalElement);
-              modal.hide();
-              confirmButton.style.display = 'none';
-              localStorage.setItem('confirmed', true);
-            });
-          }
-        }, function(error) {
-          console.log('FAILED...', error);
-        });
-    });
+  const confirmButton = document.getElementById('confirm-button');
+
+  if (localStorage.getItem('confirmed')) {
+    confirmButton.style.display = 'none';
+  }
+
+  document.getElementById('modal-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name-input').value;
+    const message = document.getElementById('message-input').value;
+    const attendance = document.querySelector('input[name="attendance"]:checked').value;
+
+    // Configura tus variables para EmailJS
+    const templateParams = {
+      user_name: name,
+      user_message: message, // Añade el mensaje
+      user_attendance: attendance
+    };
+
+    // Enviar el correo electrónico usando EmailJS
+    emailjs.send('service_l78bsuf', 'template_91koe6d', templateParams)
+      .then(function(response) {
+        if (attendance === 'Sí') {
+          Swal.fire('¡Gracias, ' + name + '!', 'Nos alegra saber que asistirás a nuestra boda.', 'success').then(() => {
+            const modalElement = document.getElementById('Modal-Asistencia');
+            const modal = bootstrap.Modal.getInstance(modalElement);
+            modal.hide();
+            confirmButton.style.display = 'none';
+            localStorage.setItem('confirmed', true);
+          });
+        } else {
+          Swal.fire('¡Gracias, ' + name + '!', 'Lamentamos que no puedas asistir. ¡Te echaremos de menos!', 'error').then(() => {
+            const modalElement = document.getElementById('Modal-Asistencia');
+            const modal = bootstrap.Modal.getInstance(modalElement);
+            modal.hide();
+            confirmButton.style.display = 'none';
+            localStorage.setItem('confirmed', true);
+          });
+        }
+      }, function(error) {
+        console.log('FAILED...', error);
+      });
   });
+
+  // Limitar el mensaje a 50 palabras
+  const messageInput = document.getElementById('message-input');
+  const wordCountDisplay = document.createElement('small');
+  wordCountDisplay.className = 'form-text text-muted';
+  messageInput.parentNode.appendChild(wordCountDisplay);
   
+  const maxWords = 30;
   
-  
+  function countWords(text) {
+    return text.trim().split(/\s+/).filter(function(word) {
+      return word.length > 0;
+    }).length;
+  }
+
+  messageInput.addEventListener('input', function() {
+    const words = countWords(messageInput.value);
+    
+    if (words > maxWords) {
+      const trimmedText = messageInput.value.split(/\s+/).slice(0, maxWords).join(' ');
+      messageInput.value = trimmedText;
+    }
+    
+    wordCountDisplay.textContent = `${countWords(messageInput.value)}/${maxWords} Máximo 30 palabras`;
+  });
+
   // Definir la fecha del evento
   const eventDate = new Date('March 23, 2025 10:00:00').getTime();
 
@@ -77,3 +102,4 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById("seconds").innerHTML = "00";
     }
   }, 1000);
+});
